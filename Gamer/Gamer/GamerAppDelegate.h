8bcc585756b2eb0927c4fac839abc9f4aedf4156
Caio Mello <caiomello254@gmail.com>
2012-12-28 16:02:53 -0200

    Project created

---
+++ Gamer/Gamer/GamerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GamerAppDelegate.h
+//  Gamer
+//
+//  Created by Caio Mello on 12/28/12.
+//  Copyright (c) 2012 Caio Mello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GamerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Gamer/Gamer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Gamer
+//
+//  Created by Caio Mello on 12/28/12.
+//  Copyright (c) 2012 Caio Mello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GamerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GamerAppDelegate class]));
+    }
+}
