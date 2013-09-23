095bfc0c16937c31554580405a55acf92b65047c
Caio Mello <caiomello254@gmail.com>
2013-09-22 22:27:17 -0300

    Image viewer rotation (iPhone)

---
+++ Gamer/Gamer/RotationLockedTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  RotationLockedTabBarController.h
+//  Gamer
+//
+//  Created by Caio Mello on 22/09/2013.
+//  Copyright (c) 2013 Caio Mello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotationLockedTabBarController : UITabBarController
+
+@end
---
+++ Gamer/Gamer/RotationLockedTabBarController.m
@@ -0,0 +1,21 @@
+//
+//  RotationLockedTabBarController.m
+//  Gamer
+//
+//  Created by Caio Mello on 22/09/2013.
+//  Copyright (c) 2013 Caio Mello. All rights reserved.
+//
+
+#import "RotationLockedTabBarController.h"
+
+@interface RotationLockedTabBarController ()
+
+@end
+
+@implementation RotationLockedTabBarController
+
+- (BOOL)shouldAutorotate{
+	return NO;
+}
+
+@end
