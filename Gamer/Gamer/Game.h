884d0e936ff3f95d2830adc9ea2ab8e3a1c6c203
Caio Mello <caiomello254@gmail.com>
2013-09-24 00:57:52 -0300

    Pre-ordered, completed, digital, loaned
    • New attributes
    • Icons
    • Interface
    • Code

--- Gamer/Gamer/Game.h
+++ Gamer/Gamer/Game.h
@@ -2,7 +2,7 @@
 //  Game.h
 //  Gamer
 //
-//  Created by Caio Mello on 15/09/2013.
+//  Created by Caio Mello on 24/09/2013.
 //  Copyright (c) 2013 Caio Mello. All rights reserved.
 //
 
@@ -26,6 +26,10 @@
 @property (nonatomic, retain) NSData * thumbnailLarge;
 @property (nonatomic, retain) NSString * title;
 @property (nonatomic, retain) NSNumber * wanted;
+@property (nonatomic, retain) NSNumber * preordered;
+@property (nonatomic, retain) NSNumber * completed;
+@property (nonatomic, retain) NSNumber * digital;
+@property (nonatomic, retain) NSNumber * loaned;
 @property (nonatomic, retain) CoverImage *coverImage;
 @property (nonatomic, retain) NSSet *developers;
 @property (nonatomic, retain) NSSet *franchises;
