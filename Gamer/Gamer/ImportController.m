7c7b9646364c06b33dbe638df0e31f7b9a1d41c7
Caio Mello <objectivecaio@gmail.com>
2014-07-20 00:49:47 -0300

    Redesigned import screen

--- Gamer/Gamer/ImportController.m
+++ Gamer/Gamer/ImportController.m
@@ -14,7 +14,12 @@
 #import "Region.h"
 #import "NSArray+Split.h"
 
-@interface ImportController ()
+@interface ImportController () <UIAlertViewDelegate>
+
+@property (nonatomic, strong) IBOutlet UILabel *progressLabel;
+@property (nonatomic, strong) IBOutlet UIProgressView *progressView;
+
+@property (nonatomic, assign) NSInteger numberOfImagesDownloaded;
 
 @property (nonatomic, strong) NSMutableArray *importedGames;
 
@@ -33,100 +38,17 @@
 	
 	self.imageCache = [NSCache new];
 	
-	NSDictionary *importedDictionary = [NSJSONSerialization JSONObjectWithData:self.backupData options:0 error:nil];
-	NSLog(@"%@", importedDictionary);
+	[self.progressLabel setText:@"Waiting..."];
+	[self.progressView setProgress:0];
 	
-	if (importedDictionary[@"games"] != [NSNull null]){
-		self.importedGames = [[NSMutableArray alloc] initWithCapacity:[importedDictionary[@"games"] count]];
-		
-		for (NSDictionary *dictionary in importedDictionary[@"games"]){
-			NSNumber *identifier = [Tools integerNumberFromSourceIfNotNull:dictionary[@"id"]];
-			Game *game = [Game MR_findFirstByAttribute:@"identifier" withValue:identifier inContext:self.context];
-			if (!game) game = [Game MR_createInContext:self.context];
-			[game setIdentifier:identifier];
-			[game setTitle:[Tools stringFromSourceIfNotNull:dictionary[@"title"]]];
-			[game setFinished:[Tools booleanNumberFromSourceIfNotNull:dictionary[@"finished"] withDefault:NO]];
-			[game setDigital:[Tools booleanNumberFromSourceIfNotNull:dictionary[@"digital"] withDefault:NO]];
-			[game setLent:[Tools booleanNumberFromSourceIfNotNull:dictionary[@"lent"] withDefault:NO]];
-			[game setPreordered:[Tools booleanNumberFromSourceIfNotNull:dictionary[@"preordered"] withDefault:NO]];
-			[game setBorrowed:[Tools booleanNumberFromSourceIfNotNull:dictionary[@"borrowed"] withDefault:NO]];
-			[game setRented:[Tools booleanNumberFromSourceIfNotNull:dictionary[@"rented"] withDefault:NO]];
-			[game setPersonalRating:[Tools integerNumberFromSourceIfNotNull:dictionary[@"personalRating"]]];
-			[game setNotes:[Tools stringFromSourceIfNotNull:dictionary[@"notes"]]];
-			if ([game.notes isEqualToString:@"(null)"]) [game setNotes:nil];
-			[game setInWishlist:[Tools booleanNumberFromSourceIfNotNull:dictionary[@"inWishlist"] withDefault:NO]];
-			[game setInLibrary:[Tools booleanNumberFromSourceIfNotNull:dictionary[@"inLibrary"] withDefault:NO]];
-			
-			if (dictionary[@"selectedRelease"] != [NSNull null]){
-				if (dictionary[@"selectedRelease"][@"id"] != [NSNull null]){
-					Release *release = [Release MR_findFirstByAttribute:@"identifier" withValue:dictionary[@"selectedRelease"][@"id"] inContext:self.context];
-					if (!release) [Release MR_createInContext:self.context];
-					[release setIdentifier:dictionary[@"selectedRelease"][@"id"]];
-					[game addReleasesObject:release];
-					[game setSelectedRelease:release];
-				}
-			}
-			
-			// Wishlist platforms
-			if (dictionary[@"wishlistPlatform"] != [NSNull null]){
-				if (dictionary[@"wishlistPlatform"][@"id"] != [NSNull null]){
-					Platform *platform = [Platform MR_findFirstByAttribute:@"identifier" withValue:dictionary[@"wishlistPlatform"][@"id"] inContext:self.context];
-					[game setWishlistPlatform:platform];
-				}
-			}
-			
-			// Library platforms
-			if (dictionary[@"libraryPlatforms"] != [NSNull null]){
-				NSMutableArray *libraryPlatforms = [[NSMutableArray alloc] initWithCapacity:[dictionary[@"libraryPlatforms"] count]];
-				for (NSDictionary *platformDictionary in dictionary[@"libraryPlatforms"]){
-					Platform *platform = [Platform MR_findFirstByAttribute:@"identifier" withValue:platformDictionary[@"id"] inContext:self.context];
-					[libraryPlatforms addObject:platform];
-				}
-				[game setLibraryPlatforms:[NSSet setWithArray:libraryPlatforms]];
-			}
-			
-			// If backup from older version, get wishlist and library attributes from location and selected platforms
-			NSNumber *location = [Tools integerNumberFromSourceIfNotNull:dictionary[@"location"]];
-			if (location){
-				if (dictionary[@"selectedPlatforms"] != [NSNull null]){
-					NSMutableArray *selectedPlatforms = [[NSMutableArray alloc] initWithCapacity:[dictionary[@"selectedPlatforms"] count]];
-					for (NSDictionary *platformDictionary in dictionary[@"selectedPlatforms"]){
-						Platform *platform = [Platform MR_findFirstByAttribute:@"identifier" withValue:platformDictionary[@"id"] inContext:self.context];
-						[selectedPlatforms addObject:platform];
-					}
-					
-					if ([location isEqualToNumber:@(1)]){
-						[game setInWishlist:@(YES)];
-						[game setWishlistPlatform:selectedPlatforms.firstObject];
-					}
-					else{
-						[game setInLibrary:@(YES)];
-						[game setLibraryPlatforms:[NSSet setWithArray:selectedPlatforms]];
-					}
-				}
-			}
-			
-			[self.importedGames addObject:game];
-		}
-		
-		// Sort imported games by title
-		NSSortDescriptor *titleSortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"title" ascending:YES];
-		self.importedGames = [self.importedGames sortedArrayUsingDescriptors:@[titleSortDescriptor]].mutableCopy;
-		
-		// Request games
-		NSArray *splitGamesArray = [NSArray splitArray:self.importedGames componentsPerSegment:100];
-		for (NSArray *games in splitGamesArray){
-			[self requestGames:games];
-		}
-		
-		// Request releases
-		NSArray *releases = [self.importedGames valueForKey:@"selectedRelease"];
-		
-		NSArray *splitReleasesArray = [NSArray splitArray:releases componentsPerSegment:100];
-		for (NSArray *releases in splitReleasesArray){
-			[self requestReleases:releases];
-		}
-	}
+	NSDictionary *importDictionary = [NSJSONSerialization JSONObjectWithData:self.backupData options:0 error:nil];
+	
+	[self parseBackupDictionary:importDictionary];
+	
+	NSString *message = [NSString stringWithFormat:@"Information and images for all of your %lu games will be downloaded. You can save before the download is finished and refresh your games later", (unsigned long)self.importedGames.count];
+	
+	UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Import" message:message delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Start", nil];
+	[alertView show];
 }
 
 - (void)didReceiveMemoryWarning{
@@ -176,7 +98,6 @@
 	}
 	
 	[cell setBackgroundColor:[UIColor colorWithRed:.164705882 green:.164705882 blue:.164705882 alpha:1]];
-	[cell setAccessoryType:game.releaseDate ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone];
 	
 	return cell;
 }
@@ -213,6 +134,9 @@
 			}
 		}
 		
+		CGFloat progress = 0.2 / (1 + self.importedGames.count / 100);
+		[self.progressView setProgress:self.progressView.progress + progress animated:YES];
+		
 		[self.navigationItem.rightBarButtonItem setEnabled:YES];
 	}];
 	[dataTask resume];
@@ -240,6 +164,9 @@
 				}
 			}
 		}
+		
+		CGFloat progress = 0.2 / (1 + self.importedGames.count / 100);
+		[self.progressView setProgress:self.progressView.progress + progress animated:YES];
 	}];
 	[dataTask resume];
 }
@@ -264,7 +191,7 @@
 			__block UIImage *image = [UIImage imageWithContentsOfFile:path];
 			
 			dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-				CGSize coverImageSize = [Tools deviceIsiPhone] ? CGSizeMake(280, 200) : CGSizeMake(420, 300);
+				CGSize coverImageSize = [Session coverImageSize];
 				
 				CGSize imageSize = image.size.width > image.size.height ? [Tools sizeOfImage:image aspectFitToWidth:coverImageSize.width] : [Tools sizeOfImage:image aspectFitToHeight:coverImageSize.height];
 				
@@ -280,13 +207,143 @@
 					
 					[game setImagePath:path];
 					[game setImageURL:URLString];
-					
-					[self.tableView reloadData];
 				});
 			});
 		}
+		
+		self.numberOfImagesDownloaded++;
+		
+		if (self.numberOfImagesDownloaded == self.importedGames.count){
+			[self.progressLabel setText:@"All done!"];
+			[self.progressView setProgress:1.0 animated:YES];
+			
+			[self.tableView reloadData];
+		}
+		else if (self.progressView.progress >= 0.4){
+			[self.progressLabel setText:@"Downloading images..."];
+			
+			CGFloat progress = 0.4 + 0.6 * ((CGFloat)self.numberOfImagesDownloaded/(CGFloat)self.importedGames.count);
+			[self.progressView setProgress:progress animated:YES];
+		}
 	}];
 	[downloadTask resume];
+}
+
+#pragma mark - AlertView
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+	if (buttonIndex == alertView.cancelButtonIndex){
+		[self.context rollback];
+		[self dismissViewControllerAnimated:YES completion:nil];
+	}
+	else{
+		[self startDownloads];
+	}
+}
+
+#pragma mark - Custom
+
+- (void)parseBackupDictionary:(NSDictionary *)importDictionary{
+//	NSLog(@"%@", importDictionary);
+	
+	if (importDictionary[@"games"] && importDictionary[@"games"] != [NSNull null]){
+		self.importedGames = [[NSMutableArray alloc] initWithCapacity:[importDictionary[@"games"] count]];
+		
+		for (NSDictionary *dictionary in importDictionary[@"games"]){
+			NSNumber *identifier = [Tools integerNumberFromSourceIfNotNull:dictionary[@"id"]];
+			Game *game = [Game MR_findFirstByAttribute:@"identifier" withValue:identifier inContext:self.context];
+			if (!game) game = [Game MR_createInContext:self.context];
+			[game setIdentifier:identifier];
+			[game setTitle:[Tools stringFromSourceIfNotNull:dictionary[@"title"]]];
+			[game setFinished:[Tools booleanNumberFromSourceIfNotNull:dictionary[@"finished"] withDefault:NO]];
+			[game setDigital:[Tools booleanNumberFromSourceIfNotNull:dictionary[@"digital"] withDefault:NO]];
+			[game setLent:[Tools booleanNumberFromSourceIfNotNull:dictionary[@"lent"] withDefault:NO]];
+			[game setPreordered:[Tools booleanNumberFromSourceIfNotNull:dictionary[@"preordered"] withDefault:NO]];
+			[game setBorrowed:[Tools booleanNumberFromSourceIfNotNull:dictionary[@"borrowed"] withDefault:NO]];
+			[game setRented:[Tools booleanNumberFromSourceIfNotNull:dictionary[@"rented"] withDefault:NO]];
+			[game setPersonalRating:[Tools integerNumberFromSourceIfNotNull:dictionary[@"personalRating"]]];
+			[game setNotes:[Tools stringFromSourceIfNotNull:dictionary[@"notes"]]];
+			if ([game.notes isEqualToString:@"(null)"]) [game setNotes:nil];
+			[game setInWishlist:[Tools booleanNumberFromSourceIfNotNull:dictionary[@"inWishlist"] withDefault:NO]];
+			[game setInLibrary:[Tools booleanNumberFromSourceIfNotNull:dictionary[@"inLibrary"] withDefault:NO]];
+			
+			if (dictionary[@"selectedRelease"] && dictionary[@"selectedRelease"] != [NSNull null]){
+				if (dictionary[@"selectedRelease"][@"id"] && dictionary[@"selectedRelease"][@"id"] != [NSNull null]){
+					Release *release = [Release MR_findFirstByAttribute:@"identifier" withValue:dictionary[@"selectedRelease"][@"id"] inContext:self.context];
+					if (!release) [Release MR_createInContext:self.context];
+					[release setIdentifier:dictionary[@"selectedRelease"][@"id"]];
+					[game addReleasesObject:release];
+					[game setSelectedRelease:release];
+				}
+			}
+			
+			// Wishlist platforms
+			if (dictionary[@"wishlistPlatform"] && dictionary[@"wishlistPlatform"] != [NSNull null]){
+				if (dictionary[@"wishlistPlatform"][@"id"] && dictionary[@"wishlistPlatform"][@"id"] != [NSNull null]){
+					Platform *platform = [Platform MR_findFirstByAttribute:@"identifier" withValue:dictionary[@"wishlistPlatform"][@"id"] inContext:self.context];
+					[game setWishlistPlatform:platform];
+				}
+			}
+			
+			// Library platforms
+			if (dictionary[@"libraryPlatforms"] && dictionary[@"libraryPlatforms"] != [NSNull null]){
+				NSMutableArray *libraryPlatforms = [[NSMutableArray alloc] initWithCapacity:[dictionary[@"libraryPlatforms"] count]];
+				for (NSDictionary *platformDictionary in dictionary[@"libraryPlatforms"]){
+					Platform *platform = [Platform MR_findFirstByAttribute:@"identifier" withValue:platformDictionary[@"id"] inContext:self.context];
+					[libraryPlatforms addObject:platform];
+				}
+				[game setLibraryPlatforms:[NSSet setWithArray:libraryPlatforms]];
+			}
+			
+			// If backup from older version, get wishlist and library attributes from location and selected platforms
+			NSNumber *location = [Tools integerNumberFromSourceIfNotNull:dictionary[@"location"]];
+			if (location){
+				if (dictionary[@"selectedPlatforms"] && dictionary[@"selectedPlatforms"] != [NSNull null]){
+					NSMutableArray *selectedPlatforms = [[NSMutableArray alloc] initWithCapacity:[dictionary[@"selectedPlatforms"] count]];
+					for (NSDictionary *platformDictionary in dictionary[@"selectedPlatforms"]){
+						Platform *platform = [Platform MR_findFirstByAttribute:@"identifier" withValue:platformDictionary[@"id"] inContext:self.context];
+						[selectedPlatforms addObject:platform];
+					}
+					
+					if ([location isEqualToNumber:@(1)]){
+						[game setInWishlist:@(YES)];
+						[game setWishlistPlatform:selectedPlatforms.firstObject];
+					}
+					else{
+						[game setInLibrary:@(YES)];
+						[game setLibraryPlatforms:[NSSet setWithArray:selectedPlatforms]];
+					}
+				}
+			}
+			
+			[self.importedGames addObject:game];
+		}
+		
+		// Sort imported games by title
+		NSSortDescriptor *titleSortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"title" ascending:YES];
+		self.importedGames = [self.importedGames sortedArrayUsingDescriptors:@[titleSortDescriptor]].mutableCopy;
+	}
+}
+
+- (void)startDownloads{
+	[self.progressLabel setText:@"Downloading info..."];
+	[self.progressView setProgress:0];
+	
+	self.numberOfImagesDownloaded = 0;
+	
+	// Request games
+	NSArray *splitGamesArray = [NSArray splitArray:self.importedGames componentsPerSegment:100];
+	for (NSArray *games in splitGamesArray){
+		[self requestGames:games];
+	}
+	
+	// Request releases
+	NSArray *releases = [self.importedGames valueForKey:@"selectedRelease"];
+	
+	NSArray *splitReleasesArray = [NSArray splitArray:releases componentsPerSegment:100];
+	for (NSArray *releases in splitReleasesArray){
+		[self requestReleases:releases];
+	}
 }
 
 #pragma mark - Actions
